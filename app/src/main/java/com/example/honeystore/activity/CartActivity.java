d695bc97f706b23c10f004a6a8fa8e06a6b59fdf
bpavle <bpavle0@gmail.com>
2021-09-28 21:39:49 +0200

    Created cart bottom and order

--- app/src/main/java/com/example/honeystore/activity/CartActivity.java
+++ app/src/main/java/com/example/honeystore/activity/CartActivity.java
@@ -9,12 +9,14 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -97,9 +99,11 @@ public class CartActivity extends AppCompatActivity {
 
         LinearLayout scrollView = (LinearLayout) findViewById(R.id.cartScrollView);
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
+        double totalCartPrice=0;
         for (final ExtendedProduct p : list) {
+            final double[] totalPriceForProduct = {p.getPrice() * p.getAmount()};
             ConstraintLayout cl = (ConstraintLayout) inflater.inflate(R.layout.cart_item_view, null);
+            cl.setId(p.getId());
             ConstraintLayout line = (ConstraintLayout) inflater.inflate(R.layout.line, null);
             ((ImageView) cl.findViewById(R.id.productImageView)).setImageResource(p.getImage());
             ((TextView) cl.findViewById(R.id.productLabel)).setText(p.getLabel());
@@ -111,7 +115,7 @@ public class CartActivity extends AppCompatActivity {
 //                intent.putExtra("productInfo",p);
 //                startActivity(intent);
 //            });
-
+System.out.println(cl.getChildAt(p.getId()));
             ((EditText)cl.findViewById(R.id.amountEditText)).addTextChangedListener(new TextWatcher() {
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -120,25 +124,49 @@ public class CartActivity extends AppCompatActivity {
 
                 @Override
                 public void onTextChanged(CharSequence s, int start, int before, int count) {
+
                     String amountString =((EditText)findViewById(R.id.amountEditText)).getText().toString();
 
                     System.out.println("AMOUNT JE "+amountString);
                     int amount = (amountString.isEmpty() ? 0 : Integer.valueOf(amountString));
-
+                    System.out.println(p.getLabel());
+                    totalPriceForProduct[0] =amount*p.getPrice();
                     ((TextView)findViewById(R.id.totalPrice)).setText(Double.toString(amount*p.getPrice()));
                 }
 
                 @Override
                 public void afterTextChanged(Editable s) {
+                    String amountString =((EditText)findViewById(R.id.amountEditText)).getText().toString();
+
 
                 }
             });
 
-
+            totalCartPrice+=totalPriceForProduct[0];
             scrollView.addView(cl);
             scrollView.addView(line);
         }
 
+        ConstraintLayout cartBottom = (ConstraintLayout) inflater.inflate(R.layout.cart_bottom_view, null);
+//        Button buttonOrder = (Button) inflater.inflate(R.id.orderButton,null);
+        Button btn = new Button(this);
+
+        btn.setText("ORDER");
+        int id = 939;
+        btn.setId(id);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CartActivity.this, "CREATED ORDER!", Toast.LENGTH_SHORT).show();
+                scrollView.removeAllViews();
+            }
+        });
+        btn.setBackgroundColor(Color.rgb(255,193,7));
+
+
+        scrollView.addView(cartBottom);
+        scrollView.addView(btn);
+        ((TextView)findViewById(R.id.totalCartPrice)).setText(Double.toString(totalCartPrice));
 
     }
 }
